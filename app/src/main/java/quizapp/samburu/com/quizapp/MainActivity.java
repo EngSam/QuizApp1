76be67db671f8eb33a2441132b38bff69f4fffc8
Sam Mburu <smburu90@gmail.com>
2018-06-30 12:34:03 +0300

    Initial commit

---
+++ app/src/main/java/quizapp/samburu/com/quizapp/MainActivity.java
@@ -0,0 +1,13 @@
+package quizapp.samburu.com.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
