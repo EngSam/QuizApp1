1138aeda6a2540337119c4bb6a8df17b9fe9ceb0
Sam Mburu <smburu90@gmail.com>
2018-07-01 22:39:19 +0300

    Store questions and answers into string vaiables

--- app/src/main/java/quizapp/samburu/com/quizapp/MainActivity.java
+++ app/src/main/java/quizapp/samburu/com/quizapp/MainActivity.java
@@ -4,22 +4,17 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int questionOne = 0;
-    int questionTwo = 0;
-    int questionThree = 0;
-    int questionFour = 0;
-    int questionFive = 0;
-    int questionSix = 0;
-    int questionSeven = 0;
-    int questionEight = 0;
-    int questionNine = 0;
-    int questionTen = 0;
+    int count = 0;
+
+    int questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitButton() {
-        int result = questionOne + questionTwo + questionThree + questionFour + questionFive + questionSix + questionSeven + questionEight + questionNine + questionTen;
-        Toast displayResultToast = Toast.makeText(this, "Your score is: " + result + "out of 10", Toast.LENGTH_LONG);
+    public void submitButton(View v) {
+
+        LinearLayout backgroundQuestionOne = findViewById(R.id.questionOneLinearLayout);
+        LinearLayout backgroundQuestionTwo = findViewById(R.id.questionTwoLinearLayout);
+        LinearLayout backgroundQuestionThree = findViewById(R.id.questionThreeLinearLayout);
+        LinearLayout backgroundQuestionFour = findViewById(R.id.questionFourLinearLayout);
+        LinearLayout backgroundQuestionFive = findViewById(R.id.questionFiveLinearLayout);
+        LinearLayout backgroundQuestionSix = findViewById(R.id.questionSixLinearLayout);
+        LinearLayout backgroundQuestionSeven = findViewById(R.id.questionSevenLinearLayout);
+        LinearLayout backgroundQuestionEight = findViewById(R.id.questionEightLinearLayout);
+        LinearLayout backgroundQuestionNine = findViewById(R.id.questionNineLinearLayout);
+        LinearLayout backgroundQuestionTen = findViewById(R.id.questionTenLinearLayout);
+        count = questionOne + questionTwo + questionThree + questionFour + questionFive + questionSix + questionSeven + questionEight + questionNine + questionTen;
+        int result = count;
+        Toast displayResultToast = Toast.makeText(this, "Your score is: " + result + " out of 10", Toast.LENGTH_LONG);
         displayResultToast.show();
     }
 
     public void questionOneAnswer(View view) {
-        LinearLayout background = findViewById(R.id.questionOneLinearLayout);
+
 
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -43,51 +50,105 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.questionOneAnswerOneRadioButton:
                 if (checked)
-                    // Set background color to red
-                    background.setBackgroundColor(Color.parseColor("#ff0000"));
+                    questionOne = 0;
                 break;
             case R.id.questionOneAnswerTwoRadioButton:
                 if (checked)
-                    // Set background color to green
-                    background.setBackgroundColor(Color.parseColor("#00ff00"));
-                questionOne = 1;
+                    questionOne = 1;
                 break;
             case R.id.questionOneAnswerThreeRadioButton:
                 if (checked)
-                    // Set background color to red
-                    background.setBackgroundColor(Color.parseColor("#ff0000"));
+                    questionOne = 0;
                 break;
             case R.id.questionOneAnswerFourRadioButton:
                 if (checked)
-                    // Set background color to red
-                    background.setBackgroundColor(Color.parseColor("#ff0000"));
+                    questionOne = 0;
                 break;
         }
 
     }
 
-    public void questionTwoAnswer() {
+    public void questionTwoAnswer(View view) {
+        RadioButton questionTwoRadioButtonOne = findViewById(R.id.questionTwoAnswerOneRadioButton);
+        RadioButton questionTwoRadioButtoTwo = findViewById(R.id.questionTwoAnswerTwoRadioButton);
+        RadioButton questionTwoRadioButtonThree = findViewById(R.id.questionTwoAnswerThreeRadioButton);
+        RadioButton questionTwoRadioButtonFour = findViewById(R.id.questionTwoAnswerFourRadioButton);
+
+        if (questionTwoRadioButtonThree.isChecked()) {
+            questionTwo = 1;
+        } else if (questionTwoRadioButtonOne.isChecked() || questionTwoRadioButtoTwo.isChecked() || questionTwoRadioButtonFour.isChecked()) {
+            questionTwo = 0;
+        }
     }
 
-    public void questionThreeAnswer() {
+    public void questionThreeAnswer(View view) {
+        CheckBox questionThreeCheckBoxOne = findViewById(R.id.questionThreeAnswerOneCheckBox);
+        CheckBox questionThreeCheckBoxTwo = findViewById(R.id.questionThreeAnswerTwoCheckBox);
+        CheckBox questionThreeCheckBoxThree = findViewById(R.id.questionThreeAnswerThreeCheckBox);
+        CheckBox questionThreeCheckBoxFour = findViewById(R.id.questionThreeAnswerFourCheckBox);
+        CheckBox questionThreeCheckBoxFive = findViewById(R.id.questionThreeAnswerFiveCheckBox);
+
+
+        if (questionThreeCheckBoxThree.isChecked() && questionThreeCheckBoxFive.isChecked() && !(questionThreeCheckBoxOne.isChecked() || questionThreeCheckBoxTwo.isChecked() || questionThreeCheckBoxFour.isChecked())) {
+
+            questionThree = 1;
+        } else if (questionThreeCheckBoxOne.isChecked() || questionThreeCheckBoxTwo.isChecked() || questionThreeCheckBoxFour.isChecked()) {
+            questionThree = 0;
+        }
+
     }
 
-    public void questionFourAnswer() {
+
+    public void questionFourAnswer(View view) {
+        RadioButton questionFourRadioButtonOne = findViewById(R.id.questionFourAnswerOneRadioButton);
+        RadioButton questionFourRadioButtonTwo = findViewById(R.id.questionFourAnswerTwoRadioButton);
+        RadioButton questionFourRadioButtonThree = findViewById(R.id.questionFourAnswerThreeRadioButton);
+        RadioButton questionFourRadioButtonFour = findViewById(R.id.questionFourAnswerFourRadioButton);
+
+        if (questionFourRadioButtonFour.isChecked()) {
+            questionFour = 1;
+        } else if (questionFourRadioButtonOne.isChecked() || questionFourRadioButtonTwo.isChecked() || questionFourRadioButtonThree.isChecked()) {
+            questionFour = 0;
+        }
     }
 
-    public void questionFiveAnswer() {
+    public void questionFiveAnswer(View view) {
+        CheckBox questionFiveCheckBoxOne = findViewById(R.id.questionFiveAnswerOneCheckBox);
+        CheckBox questionFiveCheckBoxTwo = findViewById(R.id.questionFiveAnswerTwoCheckBox);
+        CheckBox questionFiveCheckBoxThree = findViewById(R.id.questionFiveAnswerThreeCheckBox);
+        CheckBox questionFiveCheckBoxFour = findViewById(R.id.questionFiveAnswerFourCheckBox);
+        CheckBox questionFiveCheckBoxFive = findViewById(R.id.questionFiveAnswerFiveCheckBox);
+        CheckBox questionFiveCheckBoxSix = findViewById(R.id.questionFiveAnswerSixCheckBox);
+        // Only options 1,4 & 6 are valid. The rest are not.
+        if (questionFiveCheckBoxOne.isChecked() && questionFiveCheckBoxFour.isChecked() && questionFiveCheckBoxSix.isChecked() && !(questionFiveCheckBoxTwo.isChecked() || questionFiveCheckBoxThree.isChecked() || questionFiveCheckBoxFive.isChecked())) {
+            questionFive = 1;
+        } else if (questionFiveCheckBoxTwo.isChecked() || questionFiveCheckBoxThree.isChecked() || questionFiveCheckBoxFive.isChecked()) {
+            questionFive = 0;
+        }
     }
 
-    public void questionSixAnswer() {
+    public void questionSixAnswer(View view) {
     }
 
-    public void questionSevenAnswer() {
+    public void questionSevenAnswer(View view) {
     }
 
-    public void questionEightAnswer() {
+    public void questionEightAnswer(View view) {
     }
 
-    public void questionNineAnswer() {
+    public void questionNineAnswer(View view) {
+        //Create a new EditText variable, find and store the xml value into it
+        EditText questionNineAnswer = findViewById(R.id.questionNineAnswerEditText);
+        //Convert the typed answer from the EditText box in a new String variable
+        String typedAnswer = questionNineAnswer.getText().toString().toLowerCase();
+        //Create a string variable to store the correct answer
+        String correctAnswer = "multiple input multiple output";
+
+
+        if (typedAnswer.equalsIgnoreCase(correctAnswer)) {
+            questionNine = 1;
+        } else
+            questionNine = 0;
     }
 
     public void questionTenAnswer() {
